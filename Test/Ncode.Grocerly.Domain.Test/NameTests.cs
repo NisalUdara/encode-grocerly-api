1108e8aa9ea3d7b6a37c92f8c3126ff448607d2f
theNcoder <nisalwickramage@gmail.com>
2020-06-13 23:25:33 +0800

    Added tests for Name.

---
+++ Test/Ncode.Grocerly.Domain.Test/NameTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using Ncode.Grocerly.Domain.Common;
+
+namespace Ncode.Grocerly.Domain.Test
+{
+    public class NameTests
+    {
+        [Fact]
+        public void TestName()
+        {
+            Name name;
+            Action assignEmpty = () => name = (Name)string.Empty;
+            assignEmpty.Should().Throw<ArgumentException>().WithMessage("Product name has to  be non empty.");
+
+            name = (Name)"Milk";
+            name.Text.Should().Equals("Milk");
+        }
+    }
+}
